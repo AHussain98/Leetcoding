070f7e8702ca94162c71e3cca0d36a1ef3bdfaac
AHussain98 <106779657+AHussain98@users.noreply.github.com>
2023-04-18 00:12:48 +0100

    Update SearchInsertPosition.cpp

--- BinarySearch/SearchInsertPosition.cpp
+++ BinarySearch/SearchInsertPosition.cpp
@@ -1,1 +1,26 @@
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
+You must write an algorithm with O(log n) runtime complexity.
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size()-1;  // remember we're using these to store indexes
+    int mid;
+    while (low <= high){
+         mid = (low + high)/2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        if(target < nums[mid]){
+            high = mid -1;
+        }
+        else(low = mid +1);
+    }
+    return low;
+
+
+
+    }
+};
